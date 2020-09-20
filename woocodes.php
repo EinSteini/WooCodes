f3b91bdcee0cf8ce4a04f428d61acbc2174c0aa0
Your Name <you@example.com>
2020-09-20 21:09:40 +0200

    2

--- woocodes.php
+++ woocodes.php
@@ -17,13 +17,23 @@ if ( ! defined( 'ABSPATH' ) ) {
  **/
 if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
 
+    function wcs_buyevent( $order_id ) {
+
+        if ( ! $order_id )
+            return;
+
+        // Getting an instance of the order object
+        $order = wc_get_order( $order_id );
+
+    }
+    add_action('woocommerce_thankyou', 'wcs_buyevent', 10, 1);
+
     function wcs_create_category(){
         wp_insert_term( 
             'Gift Card Code',
             'product_cat'
         ) ; 
     }
-
     register_activation_hook( __FILE__, 'wcs_create_category' );
 }
 
